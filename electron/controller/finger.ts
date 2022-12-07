cb02612cbabb9846f4f6d5162217ce933a635737
qingshan777 <15054854614@163.com>
2022-12-07 16:54:59 +0800

    chore: 新增 ref-napi 等依赖

--- electron/controller/finger.ts
+++ electron/controller/finger.ts
@@ -1,8 +1,28 @@
 import ffi from 'ffi-napi'
 import { resolve } from 'path'
+import ref from 'ref-napi'
+import ArrayType from 'ref-array-napi'
+import StructType from 'ref-struct-napi'
 
-const SDK_PATH = resolve(__dirname, '../../lib/windows/x64/libzkfp')
-const fingerSDK = ffi.Library(SDK_PATH, {
-  
+const TXUSBDeviceType = StructType({
+  vid: ref.types.short,
+  pid: ref.types.short,
+  szSerialNumber: ArrayType(ref.types.char, 64),
+  bus_number: ref.types.int,
+  device_address: ref.types.int,
+  extraPtr: ref.types.uint
 })
-console.log(SDK_PATH, 'SDK_PATH')
+
+const TXUSBDeviceArrayType = ArrayType(TXUSBDeviceType)
+const deviceList = new TXUSBDeviceArrayType(16)
+
+console.log(deviceList, 'deviceList')
+
+const SDK_PATH = resolve(__dirname, '../../lib/finger/linux/x86_64/libzkfpcap.so')
+const fingerSDK = ffi.Library(SDK_PATH, {
+  sensorEnumDevices: ['int', [TXUSBDeviceArrayType, 'int']],
+  sensorRebootEx: ['int', ['int']]
+})
+
+const result = fingerSDK.sensorEnumDevices([], 16)
+console.log(result, 'sensorEnumDevices')
